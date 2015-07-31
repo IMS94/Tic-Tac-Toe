403470c838f4df015d4282f0db1be98221223a22
unknown <zeeleo7@gmail.com>
2015-07-31 17:55:04 +0530

    LeaderBoard Implementation,add LeaderBoard to Setting

--- Tik-tak-toe-pro/Process/LeaderBoard.cs
+++ Tik-tak-toe-pro/Process/LeaderBoard.cs
@@ -9,13 +9,26 @@ namespace Tik_tak_toe_pro
     //reference[0]=score.toSring()   reference[1]=name;
     public class LeaderBoard
     {
-        List<int> easyBoardScore = new List<int>();
-        List<int> mediumBoardScore = new List<int>();
-        List<int> hardBoardScore = new List<int>();
-        
-        List<string[]> easyBoardNames = new List<string[]>();
-        List<string[]> mediumBoardNames = new List<string[]>();
-        List<string[]> hardBoardNames = new List<string[]>();
+        List<int> easyBoardScore { get; set; }
+        List<int> mediumBoardScore { get; set; }
+        List<int> hardBoardScore { get; set; }
+
+        List<string[]> easyBoardNameRefs { get; set; }
+        List<string[]> mediumBoardNameRefs { get; set; }
+        List<string[]> hardBoardNameRefs { get; set; }
+
+        public LeaderBoard()
+        {
+            easyBoardScore = new List<int>();
+            mediumBoardScore = new List<int>();
+            hardBoardScore = new List<int>();
+
+            easyBoardNameRefs = new List<string[]>();
+            mediumBoardNameRefs = new List<string[]>();
+            hardBoardNameRefs = new List<string[]>();
+        }
+
+
 
         public void addNewScore(string name, int countPC, int countUser, List<string[]> boardNames,List<int> boardScore)
         {
@@ -25,27 +38,30 @@ namespace Tik_tak_toe_pro
             string[] reference = new string[2];
             reference[0] = score.ToString();
             reference[1] = name;
+            
+            //add new reference and score to lists
             boardNames.Add(reference);
             boardScore.Add(score);
-            //sort here
-        }
+            
+            //sort the list
+            boardScore.Sort();
+            
+            //if count >10 remove min
+            int minScore;
+            if (boardScore.Count > 10)
+            { 
+                minScore=boardScore[0];
+                for(int i=10;i>=0;i++)
+                {
+                    if (boardNames[i][0].Equals(minScore.ToString())) 
+                    {
+                        boardNames.RemoveAt(i);
+                        break;
+                    } 
+                }
 
-        public bool sortList(List<int[]> list, List<string> names)
-        {
-
-
-
-            if (list.Count > 10) 
-            {
-                list.RemoveAt(10);
-                names.RemoveAt(10);
-
+                boardScore.RemoveAt(0);
             }
-
         }
-
-
-
-
     }
 }
--- Tik-tak-toe-pro/Process/Setting.cs
+++ Tik-tak-toe-pro/Process/Setting.cs
@@ -9,9 +9,16 @@ namespace Tik_tak_toe_pro
     public class Setting
     {
         //difficultylevel- 1- easy  2-medium  3-hard
+        public LeaderBoard leaderBoard { set; get; }
         public int difficultyLevel{set;get;}
         public bool isStartedByPC { set; get; }
 
+        public int[] current;       //corrent[0]=total count current[1]=pcwinCount current[2]=userwincount;
+        public Setting()
+        {
+            this.leaderBoard = new LeaderBoard();
+            current = new int[3];
+        }
         
     }
 }
